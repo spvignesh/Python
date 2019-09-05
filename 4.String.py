c6712f1ca670cfc21386ecb9e7b4be45245bfff6
Vignesh <39360432+spvignesh@users.noreply.github.com>
2019-09-05 18:14:17 +0900

    Update 4.String.py

--- 4.String.py
+++ 4.String.py
@@ -5,6 +5,7 @@ print("Access values in string")
 print("var1[0]:",var1[0])
 print("var2[1:5]:",var2[1:5])
 print("---------------")
+print('\n')
 
 #Contact
 x = "Hello World!"
@@ -12,6 +13,7 @@ print( "Contact string")
 print( x[:6] )
 print( x[0:6] + "User")
 print("---------------")
+print('\n')
 
 #Python String replace() Method
 oldstring = 'I like Python' 
@@ -24,6 +26,7 @@ oldstring = 'I like Python'
 oldstring.replace('like', 'love')
 print( oldstring )
 print("---------------")
+print('\n')
 
 #Changing upper and lower case strings
 print( "Working with cases")
@@ -34,17 +37,20 @@ print( string.capitalize())
 string="PYTHON"
 print( string.lower())
 print("---------------")
+print('\n')
 
 #Using "join" function for the string
 print( "Working with join")
 print(":".join("Python"))	
 print("---------------")
+print('\n')
 	
 #Reversing String
 print( "Working with reversed")
 string="Python"		
 print(''.join(reversed(string)))
 print("---------------")
+print('\n')
 
 #Split Strings
 print( "Working with split")
@@ -53,14 +59,24 @@ print( word.split(' '))
 word="Python Development"		
 print( word.split('o'))
 print("---------------")
+print('\n')
 
 #Double the word
 print ("Working with double the word")
 x="Python Developer"
 print(x*2)
 print("---------------")
+print('\n')
+
 #Cast To string
 print( "String cast")
 x=100
 print("Python is "+str(x))
+print("---------------")
+print('\n')
 
+#string format
+print ("String format")
+name = "Python"
+number = 99
+print("%s %d" % (name,number))
