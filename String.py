a6ca606bf79c966ab02a3c541f66ceeb38eb7451
Vignesh <39360432+spvignesh@users.noreply.github.com>
2019-09-05 18:05:58 +0900

    Create String.py

---
+++ String.py
@@ -0,0 +1,66 @@
+#Accessing Values in Strings
+var1 = "Hello World"
+var2 = "Python Development"
+print("Access values in string")
+print("var1[0]:",var1[0])
+print("var2[1:5]:",var2[1:5])
+print("---------------")
+
+#Contact
+x = "Hello World!"
+print( "Contact string")
+print( x[:6] )
+print( x[0:6] + "User")
+print("---------------")
+
+#Python String replace() Method
+oldstring = 'I like Python' 
+newstring = oldstring.replace('like', 'love')
+print( "replace()")
+print( oldstring )
+print( newstring)
+
+oldstring = 'I like Python' 
+oldstring.replace('like', 'love')
+print( oldstring )
+print("---------------")
+
+#Changing upper and lower case strings
+print( "Working with cases")
+string="python"
+print( string.upper())
+string="python"		
+print( string.capitalize())
+string="PYTHON"
+print( string.lower())
+print("---------------")
+
+#Using "join" function for the string
+print( "Working with join")
+print(":".join("Python"))	
+print("---------------")
+	
+#Reversing String
+print( "Working with reversed")
+string="Python"		
+print(''.join(reversed(string)))
+print("---------------")
+
+#Split Strings
+print( "Working with split")
+word="Python Development"		
+print( word.split(' '))
+word="Python Development"		
+print( word.split('o'))
+print("---------------")
+
+#Double the word
+print ("Working with double the word")
+x="Python Developer"
+print(x*2)
+print("---------------")
+#Cast To string
+print( "String cast")
+x=100
+print("Python is "+str(x))
+
